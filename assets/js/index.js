ab3a7d7d94dbb5312fe9171387426711f1b78bf3
Victor Costa <victorcostaguitar@gmail.com>
2013-05-03 17:41:00 -0300

    testing conetion test

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,7 +3,7 @@
     // Cordova is ready
     //
     function onDeviceReady() {
-        // Empty
+        checkConnection();
     }
 
     // alert dialog dismissed
@@ -20,4 +20,20 @@
             'Game Over',            // title
             'Done'                  // buttonName
         );
+    }
+
+    function checkConnection() {
+        var networkState = navigator.connection.type;
+
+        var states = {};
+        states[Connection.UNKNOWN]  = 'Unknown connection';
+        states[Connection.ETHERNET] = 'Ethernet connection';
+        states[Connection.WIFI]     = 'WiFi connection';
+        states[Connection.CELL_2G]  = 'Cell 2G connection';
+        states[Connection.CELL_3G]  = 'Cell 3G connection';
+        states[Connection.CELL_4G]  = 'Cell 4G connection';
+        states[Connection.CELL]     = 'Cell generic connection';
+        states[Connection.NONE]     = 'No network connection';
+
+        alert('Connection type: ' + states[networkState]);
     }
