770f6c704d5bd42edb20330f7b2aa49447b9a26c
Victor Costa <victorcostaguitar@gmail.com>
2013-05-03 17:08:29 -0300

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,23 @@
+    document.addEventListener("deviceready", onDeviceReady, false);
+
+    // Cordova is ready
+    //
+    function onDeviceReady() {
+        // Empty
+    }
+
+    // alert dialog dismissed
+    function alertDismissed() {
+        // do something
+    }
+
+    // Show a custom alertDismissed
+    //
+    function showAlert() {
+        navigator.notification.alert(
+            'You are the winner!',  // message
+            alertDismissed,         // callback
+            'Game Over',            // title
+            'Done'                  // buttonName
+        );
+    }
